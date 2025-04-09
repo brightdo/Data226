50e3f2c26ef86f2f32fcab696816b2b332d5f6e4
DoHyeong(Daniel) Kim <37322269+brightdo@users.noreply.github.com>
2025-04-09 15:57:50 -0700

    Delete HW7 directory

--- HW7/build_mau/target/compiled/build_mau/models/input/session_timestamp.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-sessionId,
-ts
-FROM USER_DB_DRAGON.raw.session_timestamp
-WHERE sessionId IS NOT NULL
--- HW7/build_mau/target/compiled/build_mau/models/input/user_session_channel.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    userId,
-    sessionId,
-    channel
-FROM USER_DB_DRAGON.raw.user_session_channel
-WHERE sessionId IS NOT NULL
